ffd6adb73081fb8ef0e2de86b2774dda1dee7220
goldenstardune <goldenstar.dune@gmail.com>
2025-11-24 09:30:33 -0900

    Add files via upload

---
+++ app/handlers/__init__.py
@@ -0,0 +1,18 @@
+﻿from aiogram import Router
+
+# podroutery
+from .start import router as start_router
+from .register.first_name import router as first_name_router
+from .register.second_name import router as second_name_router
+from .register.number import router as number_router
+from app.captcha import router as captcha_router
+
+# gl router
+router = Router(name="main_handlers_router")
+router.include_router(start_router)
+router.include_router(first_name_router)
+router.include_router(second_name_router)
+router.include_router(number_router)
+router.include_router(captcha_router)
+
+__all__ = ["router"]
---
+++ app/handlers/utils.py
@@ -0,0 +1,7 @@
+﻿import re
+
+def is_valid_name(text: str) -> bool:
+    if not text or len(text.strip()) < 2:
+        return False
+    pattern = r"[A-Za-zĄĆĘŁŃÓŚŹŻąćęłńóśźż\s\-'’]+"
+    return bool(re.fullmatch(pattern, text.strip()))
