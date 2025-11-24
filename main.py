31bfc0f6b752c2cd52d120e6fff0c738f1e97521
goldenstardune <goldenstar.dune@gmail.com>
2025-11-24 09:07:01 -0900

    main.py

--- main.py
+++ main.py
@@ -1,19 +1,46 @@
 import asyncio
-from aiogram import Bot, Dispatcher, F
-from aiogram.types import InputSticker
-from app.handlers import router
-from app.database.models import async_main
+import os
+from dotenv import load_dotenv
+
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 
 
-async def main():
-	await async_main()
-	bot = Bot(token='mytoken') 
-	dp = Dispatcher()
-	dp.include_router(router) #wlaczycz router 
-	await dp.start_polling(bot)
+from app import router  #handlers
+from app.database.models import init_db #bd
+from app.middleware.antispam import RegistrationAntiSpamMiddleware
 
-if __name__ == '__main__':
-	try:
-		asyncio.run(main())
-	except KeyboardInterrupt:
-		print('I\'m unavailable right now. Let\'s chat later.')
+# .env
+load_dotenv()
+
+async def main() -> None:
+    bot_token = os.getenv("BOT_TOKEN")
+    if not bot_token:
+        raise ValueError("BOT_TOKEN nie znaleziony w pliku .env!")
+
+    bot = Bot(
+        token=bot_token,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
+
+    dp = Dispatcher()
+
+    #middleware
+    dp.message.outer_middleware(RegistrationAntiSpamMiddleware(limit=5, period=3600))
+
+    #router
+    dp.include_router(router)
+
+    await init_db()
+    print("Data baze is ready")
+
+    print("Bot started!")
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("\nI'm unavailable right now. Let's chat later.")
