0a981b9692152645ae727f58886197016a699327
goldenstardune <goldenstar.dune@gmail.com>
2025-12-14 07:50:15 -0900

    Update __init__.py

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -1,18 +1,21 @@
-﻿from aiogram import Router
+from aiogram import Router
 
-# podroutery
 from .start import router as start_router
+from .register.start import router as register_start_router         
 from .register.first_name import router as first_name_router
 from .register.second_name import router as second_name_router
 from .register.number import router as number_router
 from app.captcha import router as captcha_router
+from .register.security_question import router as security_question_router
 
-# gl router
-router = Router(name="main_handlers_router")
+router = Router(name="main_router")
+
 router.include_router(start_router)
+router.include_router(register_start_router)        
 router.include_router(first_name_router)
 router.include_router(second_name_router)
 router.include_router(number_router)
 router.include_router(captcha_router)
+router.include_router(security_question_router)
 
 __all__ = ["router"]
