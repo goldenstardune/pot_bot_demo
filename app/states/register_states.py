fd5dd4f887853005c95c40a46f78183e7c4e958d
goldenstardune <goldenstar.dune@gmail.com>
2025-12-14 08:18:29 -0900

    Update register_states.py

--- app/states/register_states.py
+++ app/states/register_states.py
@@ -1,7 +1,8 @@
 from aiogram.fsm.state import State, StatesGroup
 
 class RegisterStates(StatesGroup):
-    waiting_for_captcha = State()   
     waiting_for_first_name = State()
     waiting_for_second_name = State()
     waiting_for_number = State()
+    waiting_for_captcha = State()
+    waiting_for_security_question = State()
