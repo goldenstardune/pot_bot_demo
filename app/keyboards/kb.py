a1fc03fcdfb3b0316e17e363fd3e794026c30ef5
goldenstardune <goldenstar.dune@gmail.com>
2025-12-14 08:09:13 -0900

    Update kb.py

--- app/keyboards/kb.py
+++ app/keyboards/kb.py
@@ -2,15 +2,14 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMar
 
 main_menu = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text="Register"), KeyboardButton(text="Sign up")],
-        [KeyboardButton(text="Edit profile")]
+        [KeyboardButton(text="Register")]
     ],
     resize_keyboard=True,
     input_field_placeholder="Select an action"
 )
 
 request_number = ReplyKeyboardMarkup(
-    keyboard=[[KeyboardButton(text="Send number", request_contact=True)]],
+    keyboard=[[KeyboardButton(text="Send number 📱", request_contact=True)]],
     resize_keyboard=True,
     one_time_keyboard=True
 )
