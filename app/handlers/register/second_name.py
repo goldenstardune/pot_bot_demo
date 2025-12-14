307ec6f337e26fef8f4f75c25bfc981cd7fa96c4
goldenstardune <goldenstar.dune@gmail.com>
2025-12-14 08:05:17 -0900

    Update second_name.py

--- app/handlers/register/second_name.py
+++ app/handlers/register/second_name.py
@@ -1,4 +1,5 @@
-﻿from aiogram import Router
+from aiogram import Router
+from aiogram.types import Message, ReplyKeyboardRemove
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 
@@ -8,17 +9,19 @@ from app.keyboards.kb import request_number
 
 router = Router()
 
+
 @router.message(RegisterStates.waiting_for_second_name)
 async def second_name_handler(message: Message, state: FSMContext):
     name = message.text.strip()
     if not is_valid_name(name):
         await message.answer(
-            "Invalid last name!\n"
-            "Only letters, spaces, hyphens, and apostrophes allowed.\n"
-            "Please try again:"
+         "Nieprawidłowe nazwisko!\n"
+         "Dozwolone są tylko litery, spacje, łączniki i apostrofy.\n"
+         "Spróbuj ponownie:",
+         reply_markup=ReplyKeyboardRemove()
         )
         return
 
     await state.update_data(second_name=name)
-    await message.answer("Send your number:", reply_markup=request_number)
+    await message.answer("Podaj swój numer:", reply_markup=request_number)
     await state.set_state(RegisterStates.waiting_for_number)
